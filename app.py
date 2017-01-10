fd4217bda314bbf96d0d29a67d3d512f8ac77139
Stiven Peter <Peter@Stivens-MBP.fios-router.home>
2017-01-09 20:02:13 -0500

    Updated flask

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
