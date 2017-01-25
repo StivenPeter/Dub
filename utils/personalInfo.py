d145b5cd306948f373b942c32f5afa0285951ce2
ogal7 <ogallager@stuy.edu>
2017-01-24 22:10:38 -0500

    Update personalInfo.py

--- utils/personalInfo.py
+++ utils/personalInfo.py
@@ -37,39 +37,89 @@ def getData(username):
 	db.close()
 	return r
 
-def getIntrestList(username):
+def getInterestList(username):
         userData=getData(username)
         data=[]
-        data.append(userData[1])
-        data.append(userData[2])
-        data.append(userData[3])
+        for x in range(1, 4):
+    		if userData[x] == "bio":
+    			data.append("Biology")
+    		if userData[x] == "chem":
+    			data.append("Chemistry")
+    		if userData[x] == "math":
+    			data.append("Math")
+    		if userData[x] == "history":
+    			data.append("History")
+    		if userData[x] == "physics":
+    			data.append("Physics")
+    		if userData[x] == "compsci":
+    			data.append("Computer Science")
+    		if userData[x] == "art":
+    			data.append("Art")
+    		if userData[x] == "music":
+    			data.append("Music")
+    		if userData[x] == "travel":
+    			data.append("Travel")
+    		if userData[x] == "popcult":
+    			data.append("Pop Culture")
+    		if userData[x] == "sports":
+    			data.append("Sports")
+    		if userData[x] == "lit":
+    			data.append("Literature")
+    		if userData[x] == "food":
+    			data.append("Food")
+    		if userData[x] == "pol":
+    			data.append("Politics")
+    		if userData[x] == "fem":
+    			data.append("Feminism")
+    		if userData[x] == "memes":
+    			data.append("Memes")
         return data
+
 
 def getHobbyList(username):
         userData=getData(username)
-        data=[]
-        data.append(userData[13])
-        data.append(userData[14])
-        data.append(userData[15])
+        hobbies = ['anime', 'comedy', 'eating', 'bike', 'hike', 'walks', 'hacking', 'music', 'instrument', 'sleeping', 'reading', 'sports', 'drama', 'animals', 'culinary', 'knitting', 'diy', 'concerts']
+        hobbies2 = ['Watching Anime', 'Watching Comedy TV', 'Eating', 'Biking', 'Hiking', 'Taking Walks', 'Hacking', 'Listening to Music', 'Playing an Instrument', 'Sleeping', 'Reading', 'Playing Sports', 'Watching Drama T.V.', 'Playing with Animals', 'Cooking/Baking', 'Knitting', 'DIY', 'Going to Concerts']
+        indexes = []
+        data = []
+        for x in range(13,16):
+            indexes.append(hobbies.index(userData[x]))
+        for i in indexes:
+            data.append(hobbies2[i])
         return data
 
+
 def getPolitic(username):
-        userData=getData(username)
-        return userData[16]
+    userData=getData(username)
+    politics = ['fiscalCon', 'mainCons', 'liberal', 'socCon', 'moderate', 'socialist', 'libertarian', 'green', 'na']
+    politics2= ['Fiscal Conservative', 'Mainstream Conservative', 'Liberal', 'Social Conservative', 'Moderate', 'Socialist', 'Libertarian', 'Green Party', ' ']
+    userData = getData(username)
+    i = politics.index(userData[16])
+    return politics2[i]
+
 
 def getBig(username):
+        bigthings = ['loyal', 'stable', 'laugh', 'intel', 'sex', 'emo', 'spont', 'study']
+        bigthings2 = ['Loyalty', 'Stability', 'Laughter', 'Intellectual Stimulation', 'Sex', 'A Deep Emotional Connection', 'Spontaneity', 'A Study Buddy']
         userData=getData(username)
-        return userData[4]
+        i = bigthings.index(userData[4])
+        return bigthings2[i]
 
 def getZip(username):
         userData=getData(username)
         return userData[5]
 
 def getGender(username):
-        userData=getData(username)
-        return userData[6]
+    userData=getData(username)
+    if userData[6] == 'woman':
+        return "Female"
+    if userData[6] == 'man':
+        return "Male"
+    if userData[6] == 'noncomforming':
+        return "Agender"
+    return userData[6]
 
-def getIntrest(username):
+def getInterest(username):
         userData=getData(username)
         return userData[8]
 
@@ -83,11 +133,40 @@ def getAgeDif(username):
 
 def getReligion(username):
         userData=getData(username)
+        if userData[10] == 'christ':
+        	return "Christian"
+        if userData[10] == 'cath':
+        	return  "Catholic"
+        if userData[10] == 'prot':
+        	return "Protestant"
+        if userData[10] == 'shi':
+        	return "Shi'a"
+        if userData[10] == 'sunni':
+        	return "Sunni"
+        if userData[10] == 'jewish':
+        	return "Jewish"
+        if userData[10] == 'atheist':
+        	return "Atheist" 
+        if userData[10] == 'agno':
+        	return "Agnostic"
+        if userData[10] == 'hind':
+        	return "Hindu"
+        if userData[10] == 'budd':
+        	return "Buddhist"
+        if userData[10] == 'baha':
+        	return "Bahaist"
+        if userData[10] == 'EO':
+        	return "Eastern Orthodox"
+        if userData[10] == 'na':
+        	return " "
         return userData[10]
+
 
 def getJob(username):
         userData=getData(username)
-        return userData[11]
+        jobs = ['acct', 'exec', 'manu', 'admin', 'franch', 'nonp', 'bank', 'gov', 'pt', 'busi', 'HC', 'retail', 'contract', 'student', 'sales','cust', 'HR', 'sci', 'diversity', 'IT', 'trans','eng', 'internships']
+        jobs2 = ['Accounting', 'Executive', 'Manufacturing', 'Administration', 'Franchise', 'Nonprofit', 'Banking', 'Government', 'Part Time', 'Business', 'Health Care', 'Retail', 'Contracting and Freelance', 'Student', 'Sales', 'Customer Service', 'Human Resources', 'Science and Biotech', 'Diversity Opportunities', 'Information Technology', 'Transportation', 'Engineering', 'Internships and College']
+        return jobs2[jobs.index(userData[11])]
 
 def getReligiousPartner(username):
         userData=getData(username)
@@ -96,7 +175,17 @@ def getReligiousPartner(username):
 def getPoliticPartner(username):
         userData=getData(username)
         return userData[12]
-        
+
+def getName(username):
+	string = ""
+	f = "data/data.db"
+	db = sqlite3.connect(f)
+	og = db.cursor()
+	blob = "SELECT fname, lname FROM users2 WHERE username=='%s'"%(username)
+	t = og.execute(blob)
+	for item in t:
+		string = string + item[0] + ' ' + item[1]
+	return string
 
 
 
@@ -112,12 +201,13 @@ def getRouteForPfp(username):
 
 def getCardInfo(username):
 	info = []
-	info.append(getAge(username))
+	info.append(getName(username))
+	info.append(getAge(username) + " year old")
 	info.append(getGender(username))
 	info.append(getPolitic(username))
 	info.append(getBig(username))
 	info.append(getReligion(username))
-	info.append(getIntrestList(username))
+	info.append(getInterestList(username))
 	info.append(getHobbyList(username))
 	info.append(getJob(username))
 	path = "../" + getRouteForPfp(username)[0]
@@ -181,6 +271,6 @@ def changeSettings(username, interestList, bigthing, zipcode, gender, age, gendp
 
 #getRouteForPfp('L')
 #print getIntrestList('billy123')
-
+#print getName("jb7")
 
 
